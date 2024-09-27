3c82842ab99467573fa2745f501eacfc422b2d87
thirdmadman <thirdmadman@gmail.com>
2024-09-27 18:15:36 +0200

    feat: add basic backend setup

---
+++ task_1/variabels.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "The AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "backend_s3_bucket_name" {
+  description = "Terraform backend (state) bucket name"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "backend_s3_state_path" {
+  description = "Terraform backend (state) file path"
+  type        = string
+  default     = "us-east-1"
+}
