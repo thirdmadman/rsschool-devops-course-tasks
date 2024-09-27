a33a41e4d6af8e1cb47e8831226c98d5bbe61ea0
thirdmadman <thirdmadman@gmail.com>
2024-09-27 18:30:51 +0200

    fix: correct variables

--- task_1/variabels.tf
+++ task_1/variabels.tf
@@ -7,11 +7,9 @@ variable "aws_region" {
 variable "backend_s3_bucket_name" {
   description = "Terraform backend (state) bucket name"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "backend_s3_state_path" {
   description = "Terraform backend (state) file path"
   type        = string
-  default     = "us-east-1"
 }
