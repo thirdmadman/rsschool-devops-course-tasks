0d725e602c4a3c6af635cd50dae442038418dbd0
thirdmadman <thirdmadman@gmail.com>
2024-09-27 18:56:08 +0200

    temp commit to test

---
+++ task_1/outputs.tf
@@ -0,0 +1,11 @@
+output "variable_aws_region" {
+  value = var.aws_region
+}
+
+output "variable_backend_s3_bucket_name" {
+  value = var.backend_s3_bucket_name
+}
+
+output "variable_backend_s3_state_path" {
+  value = var.backend_s3_state_path
+}
