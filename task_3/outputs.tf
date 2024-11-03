2b6d4e85686ec4d44ed8f87d5716da6168a149d2
thirdmadman <thirdmadman@gmail.com>
2024-11-03 22:35:34 +0100

    feat: update output

--- task_3/outputs.tf
+++ task_3/outputs.tf
@@ -16,3 +16,8 @@ output "ec2_k3s_main_instance_private_ip" {
   description = "Private IP of the k3s main instance"
   value       = aws_instance.k3s_main_ec2_instance.private_ip
 }
+
+output "ssh_command" {
+  description = "Command to ssh instance in private network"
+  value       = "ssh -A -J ec2-user@${aws_instance.ec2_instance_natgw.public_ip} ec2-user@${aws_instance.k3s_main_ec2_instance.private_ip}"
+}
